4ed9528e76d43cbe6a3b869ba704764451a5673f
Ha jeongwon <hajeongwon77@gmail.com>
2020-04-13 15:21:37 +0900

    Update a main of manager.c

---
+++ market.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "product.h"
+#include <stdlib.h>
+#include <string.h>
+
+Product a;
+
+int main(){
+	Product *a[100];
+	int menu, count = 0;
+	int delete_num;
+	#ifdef DEBUG
+		printf("DDEBUG[main.c] : call loadProduct()\n");
+	#endif
+	count = loadProduct(a, count);
+	while(1){
+		#ifdef DEBUG
+			printf("DDEBUG[main.c] : call menuProduct()\n");
+		#endif	
+		menu = menuProduct();
+		if(menu == 0) break;
+	
+		if(menu != 2 && count == 0) continue;
+		
+		if(menu == 1){
+			#ifdef DEBUG
+				printf("DDEBUG[main.c] : call readProduct()\n");
+			#endif
+			readProduct(a, count);
+		}else if(menu == 2){
+			a[count] = (Product *)malloc(sizeof(Product));
+			#ifdef DEBUG	
+				printf("DDEBUG[main.c] : call createProduct()\n");
+			#endif
+			createProduct(a,count);
+			count ++;
+		}else if(menu == 3){
+			#ifdef DEBUG
+				printf("DDEBUG[main.c] : call updateProduct()\n");
+			#endif
+			updateProduct(a, count);
+		}else if(menu == 4){
+			#ifdef DEBUG
+				printf("DDEBUG[main.c] : call loadProduct()\n");
+			#endif
+			delete_num = deleteProduct(a, count);
+			free(a[delete_num]);
+			a[delete_num] = NULL;
+			count--;
+		}else if(menu == 5){
+			#ifdef DEBUG
+				printf("DDEBUG[main.c] : call saveProduct()\n");
+			#endif
+			saveProduct(a,count);
+		}else if(menu == 6){
+			#ifdef DEBUG
+				printf("DDEBUG[main.c] : call searchProduct()\n");
+			#endif
+			searchProduct(a, count);
+		}	
+		
+	}
+
+	free(a[0]);
+return 0;
+}
